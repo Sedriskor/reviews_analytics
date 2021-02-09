fe075b2f6f61d1f0c7632ed69395e6309772fafe
Sedriskor <comodexit@gmail.com>
2021-02-09 15:53:54 +0800

    ver.3 add filtering function

--- reviews_analytics.py
+++ reviews_analytics.py
@@ -15,3 +15,15 @@ for d in data:# 丟到data裡的每一筆留言
     sum_len = sum_len + len(d)
 print('平均留言長度為', int(sum_len/len(data)),'字數')
 #記得換行 不然會在FOR的範圍內一直跑
+
+#清單的篩選
+
+new = [] # new = 留言長度小於100字的清單
+for d in data:
+    if len(d) < 100:
+        new.append(d)
+print('一共有', len(new), '留言長度小於100')#這邊是字母總數 非單字
+print(new[0])
+print(new[1])
+
+
