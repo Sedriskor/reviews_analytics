92cc2c7079bd009712f653b4164b5cefe82a8088
Sedriskor <78597096+Sedriskor@users.noreply.github.com>
2021-02-14 20:50:46 +0800

    ver.2 add progressbar

--- reviews_analytics2.py
+++ reviews_analytics2.py
@@ -1,19 +1,28 @@
 
+import time
+import progressbar
+
 
 def read_file(filename): #file read
     data = []
     count = 0
     print('請等待檔案讀取...')
+    bar = progressbar.ProgressBar(max_value=1000000)
+    bar.start() #修正全部程式運行完會跑出進度調問題
     with open (filename, 'r', encoding='utf-8-sig') as f:
         for line in f:
             data.append(line)
             count += 1
+            #time.sleep(0.1)
+            bar.update(count)
+    bar.finish() #修正全部程式運行完會跑出進度調問題
     print('檔案讀取完,總共有', len(data), '筆檔案')
     return data
 
 
 def data_anaiytics(data): #data anaiytics
     print('數據分析中，請稍等...')
+    start_time = time.time() #time模組的'time()'function    
     word_count = {} 
     for d in data:
         words = d.split() #split本身就是跳過空白鍵 如果打入' '會出現空字串
@@ -22,11 +31,14 @@ def data_anaiytics(data): #data anaiytics
                 word_count[word] += 1 
             else:
                 word_count[word] = 1 #新增新的字進去word_count字典
+
         
     # for word in word_count: #列出大於200萬次的字
-    #     if word_count[word] > 2000000:
+    #     if word_count[word] > 1000:
     #         print(word, word_count[word])
-    # # print(word_count['the'])
+    # print(word_count['the'])
+    end_time = time.time()
+    print('共花了', end_time - start_time, '秒')
     return word_count
 
 
@@ -42,8 +54,10 @@ def user_search(word_count): #user search word
     
 
 def main():
-    data = read_file('reviews.txt')
+    data = read_file('reviews.txt') 
     word_count = data_anaiytics(data)
     user_search(word_count)
 
-main()
+
+if __name__ == '__main__':
+    main()
